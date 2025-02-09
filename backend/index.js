a36a2ab00796cf0c282abe96777a27fc1eac0d95
Himanshu Tiwari <htiwari578@gmail.com>
2025-02-09 22:19:34 +0530

    updated code

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,20 @@ import express from 'express';
 
 import dotenv from "dotenv";
 import connectDB from './config/db.js';
+import router from './routes/userRoute.js';
+import cors from 'cors'
 
 
 
 const app = express();
 
 dotenv.config();
+app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
+
+app.use('/api/auth', router)
 
 app.listen(PORT, async ()=> {
     console.log(`Server running on port : ${PORT}`);
