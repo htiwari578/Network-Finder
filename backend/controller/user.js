2ba0f3f92cd53fa8f38d7b001934594dbc83bf36
Himanshu Tiwari <htiwari578@gmail.com>
2025-02-10 17:05:18 +0530

    updated

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,24 +1,25 @@
 import User from "../models/userModels.js"
 import jwt from "jsonwebtoken";
+import { sendRechargeEmail } from "../utils/emailService.js";
 
 export const login = async (req,res)=> {
     try {
         const {email} = req.body
-        let user 
-        user = await User.findOne({email});
+        let user = await User.findOne({email});
 
         if(!user){
-           const newUser = new User({
-            email
+           user = new User({
+            email,
+            credits: 5,
            })
-           await newUser.save()
-           user = newUser
+           await user.save()
+    
         }
           // Generate JWT Token
         const token = jwt.sign(
-            { id: user._id, email: user.email }, // ✅ Only store necessary user data
+            { id: user._id, email: user.email }, 
              process.env.JWT_SECRET,
-            { expiresIn: "1d" } // ✅ Token expires in 1 day
+            { expiresIn: "1d" } 
       );
         res.cookie('access_token', token, {
             httpOnly: true,
@@ -36,4 +37,29 @@ export const login = async (req,res)=> {
  
     }
     
+}
+
+
+//deduct 1 credit per serach
+export const deductCredit = async(req,res)=>{
+    try {
+        const {email} = req.body
+        let user = await User.findOne({email})
+
+        if(!user){
+            return res.status(404).json({
+                messagge:"User not found"
+            });
+        }
+        if(user.credits <= 0){
+            await sendRechargeEmail(user.email);
+            return res.status(403).json({
+                message:"Your credit exhausted, please recharge your email"
+            });
+        }
+        user.credits -= 1;
+        await user.save();
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
 }
